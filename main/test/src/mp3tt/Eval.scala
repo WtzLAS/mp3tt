2e00909946668a63880cae508256a0b0e0dbc03c
Wtz_LASR <wtz_lasr@outlook.com>
2024-04-15 02:25:59 +0800

    rename

--- main/test/src/mp3tt/Eval.scala
+++ main/test/src/mp3tt/Eval.scala
@@ -3,7 +3,7 @@ package mp3tt
 import utest.*
 import mp3tt.model.*
 
-object ElabTests extends TestSuite {
+object EvalTests extends TestSuite {
   val tests = Tests {
     test("eval1") {
       val tm = TmC.App(TmC.Lam("x", TmV.Refl, TmC.Return(TmV.Var(0))), TmV.Refl)
